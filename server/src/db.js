e2f350f1dfabfd3c5b340f557d7a920204e94d67
Gergo <halmi.gergo@gmail.com>
2019-06-17 21:11:35 +0200

    sbConenction

--- server/src/db.js
+++ server/src/db.js
@@ -2,10 +2,7 @@ const { Pool } = require('pg')
 const config = require("./config.json")
 
 const pool = new Pool({
-    user: config.db.user,
-    host: config.db.host,
-    database: config.db.database,
-    port: config.db.port
+    connectionString: process.env.DATABASE_URL
 })
 
-module.exports.pool = pool
+module.exports.pool = pool.connect()
