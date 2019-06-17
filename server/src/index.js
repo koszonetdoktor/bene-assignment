8f91f0ca75da39587d945d35a98348716f70174f
Gergo <halmi.gergo@gmail.com>
2019-06-17 20:58:00 +0200

    port cahnges

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ app.use(bodyParse.json())
 app.use(cookieParser())
 app.use(routes)
 
-let port = 5000
+let port = process.env.PORT || 5000
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("../ui/build"))
     app.get("/", (req, res) => {
